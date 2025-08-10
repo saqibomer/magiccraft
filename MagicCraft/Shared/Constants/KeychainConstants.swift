5218b8aaf18724afed21c7c4c3ffbec9acead22b
saqibomer <saqibomer.cs@gmail.com>
2025-08-10 22:50:19 +0500

    Added etherscan key constants

--- MagicCraft/Shared/Constants/KeychainConstants.swift
+++ MagicCraft/Shared/Constants/KeychainConstants.swift
@@ -8,4 +8,7 @@
 struct KeychainConstants {
     static let service = "com.kaboomlab.MagicCraft"
     static let account = "mnemonic"
+    
+    static let etherscanService = "com.magiccraft.etherscan"
+    static let etherscanAccount = "etherscan.api.key"
 }
