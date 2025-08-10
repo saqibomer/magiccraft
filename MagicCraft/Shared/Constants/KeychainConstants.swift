312748c5de064f9b39e9b520ad9a5fe91f04e90b
saqibomer <saqibomer.cs@gmail.com>
2025-08-10 13:49:55 +0500

    - Added constants

---
+++ MagicCraft/Shared/Constants/KeychainConstants.swift
@@ -0,0 +1,11 @@
+//
+//  KeychainConstants.swift
+//  MagicCraft
+//
+//  Created by TOxIC on 10/08/2025.
+//
+
+struct KeychainConstants {
+    static let service = "com.kaboomlab.MagicCraft"
+    static let account = "mnemonic"
+}
