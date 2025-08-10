c3e91d301d50fd176f44b645325e033f90e007d6
saqibomer <saqibomer.cs@gmail.com>
2025-08-10 22:50:45 +0500

    Added header View

--- MagicCraft/UI/TitleHeaderView/TitleHeaderView.swift
+++ MagicCraft/UI/TitleHeaderView/TitleHeaderView.swift
@@ -1,7 +1,16 @@
 //
-//  TitleHeaderView 2.swift
+//  TitleHeaderView.swift
 //  MagicCraft
 //
 //  Created by TOxIC on 10/08/2025.
 //
 
+import SwiftUI
+
+struct TitleHeaderView: View {
+    var body: some View {
+        Text("MagicCraft Wallet")
+            .font(.largeTitle.bold())
+            .padding(.top, 20)
+    }
+}
